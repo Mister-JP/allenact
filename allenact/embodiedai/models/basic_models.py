7596fe76cad58f812dd223c7bc90e3b665290d0c
Jignasu Pathak <jignasupathak@Jignasus-MacBook-Pro.local>
2023-11-15 17:20:24 -0500

    Added a flag

--- allenact/embodiedai/models/basic_models.py
+++ allenact/embodiedai/models/basic_models.py
@@ -290,7 +290,7 @@ class RNNStateEncoder(nn.Module):
 
     def layer_init(self):
         """Initialize the RNN parameters in the model."""
-        get_logger().info("Starting for loop")
+        get_logger().info(f"Starting for loop for {len(self.rnn.named_parameters())}")
         for name, param in self.rnn.named_parameters():
             if "weight" in name:
                 nn.init.orthogonal_(param)
