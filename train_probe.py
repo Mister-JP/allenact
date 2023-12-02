9da3ae604c9a594bb5830f50bc17bf9cf166ff0f
Jignasu Pathak <jignasupathak@Jignasus-MacBook-Pro.local>
2023-12-02 10:01:17 -0500

    Improved probe

--- train_probe.py
+++ train_probe.py
@@ -1,11 +1,15 @@
 import argparse
-
+import os
+import random
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import os
+import torch.utils.data as data
 import matplotlib.pyplot as plt
-import random
+
+# Set a random seed for reproducibility
+torch.manual_seed(0)
+random.seed(0)
 
 class CoordinateMLP(nn.Module):
     def __init__(self, input_size, hidden_size=128):
@@ -24,132 +28,107 @@ class CoordinateMLP(nn.Module):
         return x
 
 def calculate_accuracy(predictions, targets, threshold=0.1):
-    # Compute the L2 distance between predictions and targets
     l2_distances = torch.norm(predictions - targets, dim=1)
-    # Compute accuracy as the percentage of distances below the threshold
     accuracy = torch.mean((l2_distances < threshold).float()).item()
     return accuracy
 
-def train_probe(data_folder, model_save_path, input_size):
+def train_probe(data_folder, model_save_path, input_size, batch_size=32, epochs=1000, learning_rate=0.001):
+    # Check if data folder exists
+    if not os.path.exists(data_folder):
+        raise FileNotFoundError(f"Data folder '{data_folder}' not found.")
+
     # Load the data
     data_file = os.path.join(data_folder, 'inference_data.pt')
+    if not os.path.exists(data_file):
+        raise FileNotFoundError(f"Data file '{data_file}' not found.")
     data = torch.load(data_file)
 
-    # Split the data into training and testing sets (80-20 split)
+    # Split the data into training, validation, and testing sets
     random.shuffle(data)
-    split_index = int(0.8 * len(data))
-    train_data = data[:split_index]
-    print(len(train_data))
-    print(len(data))
-    test_data = data[split_index:]
+    train_size = int(0.7 * len(data))
+    valid_size = int(0.15 * len(data))
+    train_data, valid_data, test_data = data[:train_size], data[train_size:train_size+valid_size], data[train_size+valid_size:]
 
+    # Create data loaders
+    train_loader = data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    valid_loader = data.DataLoader(valid_data, batch_size=batch_size)
+    test_loader = data.DataLoader(test_data, batch_size=batch_size)
 
     # Initialize the probe model
     probe = CoordinateMLP(input_size)
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(probe.parameters(), lr=0.001)
+    optimizer = optim.Adam(probe.parameters(), lr=learning_rate)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.1)  # Adjusts learning rate
 
-    loss_history = []
-    accuracy_history = []
-    test_loss_history = []
-    test_accuracy_history = []
-
-    # Training loop
-    for epoch in range(1000):  # number of epochs can be adjusted
+    best_valid_loss = float('inf')
+    for epoch in range(epochs):
+        probe.train()
         running_loss = 0.0
         running_acc = 0.0
-        for i, data_pair in enumerate(train_data):
-            # Zero the parameter gradients
-            optimizer.zero_grad()
 
-            # Forward pass
+        for data_pair in train_loader:
+            optimizer.zero_grad()
             inputs = data_pair['memory_tensor']
             target_coordinates = data_pair['target_coordinates_ind']
             outputs = probe(inputs)
-
-            # Compute loss
             loss = criterion(outputs, target_coordinates)
             loss.backward()
             optimizer.step()
-
-            # Print statistics
-            # Compute accuracy
-            accuracy = calculate_accuracy(outputs, target_coordinates)
-            running_acc += accuracy
-            accuracy_history.append(accuracy)
-
-            # Print statistics
-            # print(f'[{epoch + 1}, {i + 1:5d}] loss: {loss.item():.3f}, accuracy: {accuracy:.3f}')
             running_loss += loss.item()
+            running_acc += calculate_accuracy(outputs, target_coordinates)
 
-        avg_train_loss = running_loss / len(train_data)
-        loss_history.append(avg_train_loss)
-        avg_train_accuracy = running_acc / len(test_data)
-        # Evaluate on the testing set
-        probe.eval()  # Set the model to evaluation mode
-        with torch.no_grad():  # Disable gradient calculation
-            running_test_loss = 0.0
-            total_test_accuracy = 0.0
-            for i, data_pair in enumerate(test_data):
+        # Validation
+        probe.eval()
+        valid_loss = 0.0
+        valid_acc = 0.0
+        with torch.no_grad():
+            for data_pair in valid_loader:
                 inputs = data_pair['memory_tensor']
                 target_coordinates = data_pair['target_coordinates_ind']
                 outputs = probe(inputs)
-                test_loss = criterion(outputs, target_coordinates)
-                running_test_loss += test_loss.item()
-                test_accuracy = calculate_accuracy(outputs, target_coordinates)
-                total_test_accuracy += test_accuracy
+                valid_loss += criterion(outputs, target_coordinates).item()
+                valid_acc += calculate_accuracy(outputs, target_coordinates)
 
-            avg_test_loss = running_test_loss / len(test_data)
-            avg_test_accuracy = total_test_accuracy / len(test_data)
-            test_loss_history.append(avg_test_loss)
-            test_accuracy_history.append(avg_test_accuracy)
+        # Update learning rate
+        scheduler.step()
 
-        # Print training and testing statistics
-        print(f'Epoch {epoch + 1}: Train Loss: {avg_train_loss:.3f}, Train Accuracy: {avg_train_accuracy:.3f}, Test Loss: {avg_test_loss:.3f}, Test Accuracy: {avg_test_accuracy:.3f}')
-            # print(f'[{epoch + 1}, {i + 1:5d}] loss: {loss.item() / 10:.3f}')
-    # Plotting and saving the graphs
-    plt.figure(figsize=(12, 6))
+        # Early stopping based on validation loss
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            torch.save(probe.state_dict(), model_save_path)
 
-    # Plot loss
-    plt.subplot(1, 2, 1)
-    plt.plot(loss_history, label='Loss')
-    plt.title('Training Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
+        print(f'Epoch {epoch+1}/{epochs}: Train Loss: {running_loss/len(train_loader):.4f}, Train Acc: {running_acc/len(train_loader):.4f}, Valid Loss: {valid_loss/len(valid_loader):.4f}, Valid Acc: {valid_acc/len(valid_loader):.4f}')
 
-    # Plot accuracy
-    plt.subplot(1, 2, 2)
-    plt.plot(accuracy_history, label='Accuracy', color='orange')
-    plt.title('Training Accuracy')
-    plt.xlabel('Iteration')
-    plt.ylabel('Accuracy')
-    plt.legend()
+    # Load the best model
+    probe.load_state_dict(torch.load(model_save_path))
 
-    plt.tight_layout()
+    # Test the model
+    probe.eval()
+    test_loss = 0.0
+    test_acc = 0.0
+    with torch.no_grad():
+        for data_pair in test_loader:
+            inputs = data_pair['memory_tensor']
+            target_coordinates = data_pair['target_coordinates_ind']
+            outputs = probe(inputs)
+            test_loss += criterion(outputs, target_coordinates).item()
+            test_acc += calculate_accuracy(outputs, target_coordinates)
 
-    # Save the figures
-    loss_plot_path = os.path.join(data_folder, f'{VERSION}_loss.png')
-    accuracy_plot_path = os.path.join(data_folder, f'{VERSION}_accuracy.png')
-    plt.savefig(loss_plot_path)
-    plt.savefig(accuracy_plot_path)
-
-    print('Loss and accuracy plots saved.')
-            
-    # Save the trained model
-    torch.save(probe.state_dict(), model_save_path)
-    print('Finished Training. Model saved to', model_save_path)
+    print(f'Test Loss: {test_loss/len(test_loader):.4f}, Test Acc: {test_acc/len(test_loader):.4f}')
 
 # Argument parsing
 parser = argparse.ArgumentParser(description='Train CoordinateMLP Probe')
-parser.add_argument('VERSION', type=str, help='Version string for the run')
+parser.add_argument('--version', type=str, required=True, help='Version string for the run')
+parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
+parser.add_argument('--epochs', type=int, default=1000, help='Number of epochs for training')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate for the optimizer')
 args = parser.parse_args()
 
 # Parameters
-VERSION = args.VERSION
+VERSION = args.version
 data_folder = f'probe_data/{VERSION}'  # replace with your actual folder path
 model_save_path = os.path.join(data_folder, 'probe_model.pth')
 input_size = 512  # the input size to the MLP probe, as per your architecture
 
 # Train the probe
-train_probe(data_folder, model_save_path, input_size)
+train_probe(data_folder, model_save_path, input_size, args.batch_size, args.epochs, args.learning_rate)
